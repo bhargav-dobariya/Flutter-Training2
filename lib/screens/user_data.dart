cb01e8facd221ffab2487b2a5b04ed45555550ba
bhargav-dobariya <bhargavdobariya06@gmail.com>
2022-03-30 12:57:05 +0530

    Http Post Completed

--- lib/screens/user_data.dart
+++ lib/screens/user_data.dart
@@ -1,27 +1,10 @@
-import 'package:alert_dialog/model_class.dart';
 // import 'package:alert_dialog/model_class.dart';
 import 'package:flutter/material.dart';
 // import 'package:alert_dialog/model_class.dart';
 // import 'package:flutter/material.dart';
 // import 'package:convert/convert.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-@override
 class UserData extends StatefulWidget {
   UserData({Key? key}) : super(key: key);
 
@@ -30,13 +13,15 @@ class UserData extends StatefulWidget {
 }
 
 class _UserDataState extends State<UserData> {
-  String? data;
-  late Future<Album> futureAlbum;
+  final url = "https://jsonplaceholder.typicode.com/posts";
+  void postData() async {
+    try {
+      final response = await post(Uri.parse(url),
+          body: {"title": "Anything", "body": "Post body", "userId": "1"});
 
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
+      print(response.body);
+    } catch (er) {}
+    ;
   }
 
   @override
@@ -51,26 +36,7 @@ class _UserDataState extends State<UserData> {
           title: const Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  children: [
-                    Text(snapshot.data!.data!.firstName.toString()),
-                    Text(snapshot.data!.data!.lastName.toString()),
-                    Text(snapshot.data!.data!.id.toString()),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
+            child: ElevatedButton(onPressed: postData, child: Text("Post"))),
       ),
     );
   }
