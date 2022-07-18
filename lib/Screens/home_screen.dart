7e9ddb6736ae8c26decddae12056053d4aad16ba
Bhargav <bhargavdobariya06@gmail.com>
2022-07-18 19:12:06 +0530

    implemented sneak peek view

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -9,13 +9,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List profieleImage = [
-    "assets/bedroom1.jpeg",
-    "assets/bedroom2.webp",
-    "assets/bedroom3.jpeg",
-    "assets/bedroom1.jpeg",
-    "assets/bedroom2.webp",
-    "assets/bedroom3.jpeg",
+  List gameImage1 = [
+    "assets/game_1.jpeg",
+    "assets/game_6.png",
+    "assets/game_2.jpg",
+    "assets/game_3.jpeg",
+    "assets/game_4.png",
+    "assets/game_5.webp",
+    "assets/game_3.jpeg",
+    "assets/game_5.webp",
+    "assets/game_6.png",
+  ];
+  List gameTitle1 = [
+    "San Chin's Adventure",
+    "Clash of Clans",
+    "Temple Run",
+    "Battle Ground Mobile India",
+    "Spider Man",
+    "BGMI",
+    "Mini Millatry"
+        "Clash Of Kings",
+    "Hay - Day"
   ];
   late double _deviceHeight;
   late double _deviceWidth;
@@ -31,55 +45,211 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildUi() {
     return Scaffold(
-      backgroundColor: ColorAsset.whitecolor,
-      key: scaffoldKey,
-      body: _bodyUi(),
-    );
+        backgroundColor: ColorAsset.blackcolor,
+        key: scaffoldKey,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(left: _deviceWidth * 0.02),
+            child: Column(
+              children: [
+                _gridOne(),
+                SizedBox(
+                  height: _deviceHeight * 0.025,
+                ),
+                _gridTwo(),
+                SizedBox(
+                  height: _deviceHeight * 0.025,
+                ),
+                _gridThree()
+              ],
+            ),
+          ),
+        ));
   }
 
-  Widget _bodyUi() {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topCenter,
-          child: Text(
-            "Games",
-            style: TextStyle(color: ColorAsset.whitecolor),
-          ),
+  Widget _gridOne() {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      Container(
+        margin: EdgeInsets.only(bottom: _deviceHeight * 0.02),
+
+        // alignment: Alignment.topCenter,
+        child: Text(
+          "Games",
+          style: TextStyle(color: ColorAsset.whitecolor),
         ),
-        Container(
-          alignment: Alignment.topLeft,
-          child: Text(
-            "What We're Playing",
-            style: TextStyle(color: ColorAsset.whitecolor),
-          ),
+      ),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text(
+          "What We're Playing",
+          style: TextStyle(
+              color: ColorAsset.whitecolor,
+              fontSize: _deviceHeight * 0.025,
+              fontWeight: FontWeight.bold),
         ),
-        Expanded(
-          child: SizedBox(
-            height: 100,
-            child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemCount: 3,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    children: [
-                      ListView.builder(
-                          physics: const ClampingScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 6,
-                          itemBuilder: (BuildContext context, int index) {
-                            return ListTile(
-                              leading: Image.asset(profieleImage[index]),
-                              title: const Text("Candy crush"),
-                            );
-                          })
-                    ],
-                  );
-                }),
+      ),
+      SizedBox(
+          height: _deviceHeight * 0.26,
+          child: GridView.builder(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: _deviceHeight * 0.047 / _deviceWidth * 2,
+              mainAxisSpacing: _deviceWidth * 0.032,
+            ),
+            itemCount: gameTitle1.length,
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Image.asset(
+                        gameImage1[index],
+                        height: 60.0,
+                        width: 60.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(
+                      width: _deviceWidth * 0.58,
+                      child: Text(
+                        gameTitle1[index],
+                        style: TextStyle(color: ColorAsset.whitecolor),
+                      ),
+                    ),
+                    Container(
+                      child: Icon(
+                        Icons.download,
+                        color: ColorAsset.blueColor,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          )),
+    ]);
+  }
+
+  Widget _gridTwo() {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      Container(
+        margin: EdgeInsets.only(bottom: _deviceHeight * 0.02),
+        alignment: Alignment.topLeft,
+        child: Text(
+          "Top Apple Arcade Games For You",
+          style: TextStyle(
+              color: ColorAsset.whitecolor,
+              fontSize: _deviceHeight * 0.025,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      SizedBox(
+        height: _deviceHeight * 0.26,
+        child: GridView.builder(
+          scrollDirection: Axis.horizontal,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: _deviceHeight * 0.047 / _deviceWidth * 2,
+            mainAxisSpacing: _deviceWidth * 0.032,
           ),
-        )
-      ],
-    );
+          itemCount: 6,
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 1,
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.asset(
+                      gameImage1[index],
+                      height: 60.0,
+                      width: 60.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    width: _deviceWidth * 0.58,
+                    child: Text(
+                      gameTitle1[index],
+                      style: TextStyle(color: ColorAsset.whitecolor),
+                    ),
+                  ),
+                  Container(
+                    child: Icon(
+                      Icons.download,
+                      color: ColorAsset.blueColor,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    ]);
+  }
+
+  Widget _gridThree() {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      Container(
+        margin: EdgeInsets.only(bottom: _deviceHeight * 0.02),
+        alignment: Alignment.topLeft,
+        child: Text(
+          "Recomended for You",
+          style: TextStyle(
+              color: ColorAsset.whitecolor,
+              fontSize: _deviceHeight * 0.025,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      SizedBox(
+        height: _deviceHeight * 0.26,
+        child: GridView.builder(
+          scrollDirection: Axis.horizontal,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: _deviceHeight * 0.047 / _deviceWidth * 2,
+            mainAxisSpacing: _deviceWidth * 0.032,
+          ),
+          itemCount: gameTitle1.length,
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 1,
+              // width: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.asset(
+                      gameImage1[index],
+                      height: 60.0,
+                      width: 60.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    width: _deviceWidth * 0.58,
+                    child: Text(
+                      gameTitle1[index],
+                      style: TextStyle(color: ColorAsset.whitecolor),
+                    ),
+                  ),
+                  Container(
+                    child: Icon(
+                      Icons.download,
+                      color: ColorAsset.blueColor,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    ]);
   }
 }
